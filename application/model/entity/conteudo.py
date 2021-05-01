cbdf42fa554cf903dda394838babde343a3d1579
Daniel Cabral <danielccabral1981@gmail.com>
2021-05-01 14:19:45 -0300

    Criando class Conteudo

---
+++ application/model/entity/conteudo.py
@@ -0,0 +1,12 @@
+
+
+class Conteudo():
+    def __init__(self, texto):
+        self.__texto = texto
+
+
+    def get_text(self):
+        return texto
+
+    def set_texto(self, texto):
+        self.__texto = texto
